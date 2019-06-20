70249bdb0630e1be052e9fcce3a0e1c2a0cb1023
Kenneth Hanley <kennethhanley@gmail.com>
2019-06-20 21:56:00 +0100

    Added fix ssuggested by dsiddons on SpadeR issue #2

--- R/SortDataFun.R
+++ R/SortDataFun.R
@@ -7,13 +7,21 @@ function(x1, x2) {
   #   sit <- sample(sit[1:D12], replace=F)  # random sample
   common <- cbind(x1[sit[1:D12]], x2[sit[1:D12]])
   sit1 <- order(x1 > 0 & x2 == 0, decreasing=T)
-  set.seed(123)
-  sit1 <- sample(sit1[1:(D1-D12)], replace=F)  # random sample
-  special1 <- cbind(x1[sit1[1:(D1-D12)]], x2[sit1[1:(D1-D12)]])
+  if (D1-D12 == 1){
+    special1 <- cbind(x1[sit1[1]], x2[sit1[1]]) 
+  } else {
+    set.seed(123)
+    sit1 <- sample(sit1[1:(D1-D12)], replace=F)  # random sample
+    special1 <- cbind(x1[sit1[1:(D1-D12)]], x2[sit1[1:(D1-D12)]])
+  }
   sit2 <- order(x1 == 0 & x2 > 0, decreasing=T)
-  set.seed(123)
-  sit2 <- sample(sit2[1:(D2-D12)], replace=F)  # random sample
-  special2 <-  cbind(x1[sit2[1:(D2-D12)]], x2[sit2[1:(D2-D12)]])
+  if (D1-D12 == 1){
+    special2 <- cbind(x1[sit2[1]], x2[sit2[1]]) 
+  } else {
+    set.seed(123)
+    sit2 <- sample(sit2[1:(D2-D12)], replace=F)  # random sample
+    special2 <-  cbind(x1[sit2[1:(D2-D12)]], x2[sit2[1:(D2-D12)]])
+  }
   z <- rbind(common, special1, special2)
   return(z)
 }
